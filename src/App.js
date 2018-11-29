4510ef174d17937742c216121d3f8d6b44e40455
Eric Laursen <ericjlaursen@gmail.com>
2018-11-29 12:56:53 -0500

    moves chess piece around automatically.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import './resetNormalize.css';
 import './main.css';
 import Board from './components/Board'
+import observe from './utils/observe'
 
-const App = () => {
-  return (
-  	<Board knightPosition={[0,0]}/>
+// const App = () => {
+//   return (
+//   	<Board knightPosition={[2,1]}/>
 
-  );
-};
-export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
+//   );
+// };
+// export default App;
+
+observe(knightPosition =>
+	ReactDOM.render(<Board knightPosition={knightPosition}/>, 
+		document.getElementById("app"))
+);
